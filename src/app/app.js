b41d009e76f8a4cae0936eb0a880eccecdfcd718
Michael Kueller <michael.kueller@zanox.com>
2013-06-04 13:57:19 +0200

    added server and header. changed Karma browser to Chrome

--- src/app/app.js
+++ src/app/app.js
@@ -17,5 +17,33 @@ angular.module( 'ngBoilerplate', [
 .controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
 })
 
+
+.controller('HeaderCtrl', ['$scope', '$location', '$route', 'security', 'breadcrumbs', 'notifications', 'httpRequestTracker',
+  function ($scope, $location, $route, security, breadcrumbs, notifications, httpRequestTracker) {
+  $scope.location = $location;
+  $scope.breadcrumbs = breadcrumbs;
+
+  $scope.isAuthenticated = security.isAuthenticated;
+  $scope.isAdmin = security.isAdmin;
+
+  $scope.artifacts = ['addeliverydashboard', 'advertiserstatistics' , 'billingcenter', 'business'];
+
+  $scope.home = function () {
+    if (security.isAuthenticated()) {
+      $location.path('/dashboard');
+    } else {
+      $location.path('/projectsinfo');
+    }
+  };
+
+  $scope.isNavbarActive = function (navBarPath) {
+    return navBarPath === breadcrumbs.getFirst().name;
+  };
+
+  $scope.hasPendingRequests = function () {
+    return httpRequestTracker.hasPendingRequests();
+  };
+}]);
+
 ;
 
